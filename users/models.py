2b9f25c1958753da0d61ac8ff2b038a2801c1dd4
assam06 <67585123+assam06@users.noreply.github.com>
2020-09-05 22:49:53 +0900

    finish user app

--- users/models.py
+++ users/models.py
@@ -18,8 +18,32 @@ class User(AbstractUser):
         (GENDER_FEMALE, "Female"),
         (GENDER_OTHER, "Other"),
     )
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+
+    LANGUAGE_CHOICES = (
+        # db로 가는 값,      #form에서 보여지는 값
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_KOREAN, "Korean"),
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
     # CharField는 1arg를 가지는데 max_length임. ** 중요! 최대 글자수를 말해. null=True는 비어있어도 괜찮다는거야.
     # form에 변화룰 준거(성별성택)같은걸로는 migration등이 필요하지 않아
-    avatar = models.ImageField(null=True. blank=True)
-    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, null=True, blank=True)
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(
+        choices=GENDER_CHOICES, max_length=10, null=True, blank=True
+    )
     bio = models.TextField(default="", blank=True)
+    birthdate = models.DateField(null=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=2, null=True, blank=True
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=3, null=True, blank=True
+    )
+    superhost = models.BooleanField(default=False)
