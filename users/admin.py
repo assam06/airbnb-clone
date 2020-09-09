61fc6771009ae193213ae2ec5bed210982b8874f
assam06 <67585123+assam06@users.noreply.github.com>
2020-09-09 14:55:06 +0900

    User Admin

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
-# Register your models here.
+# 여기에 추가하는걸로 확장할 수 있어
+# 데코레이터는 클래스 위에 있어야 작동해. 얘는 게시판 위에 분류 만들어줌
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
-    pass
+class CustomUserAdmin(UserAdmin):
+
+    """ Custom User Admin  """
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
